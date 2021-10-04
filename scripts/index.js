0d526b61c80022805d4e8e1d9b5405c87067e534
Tim Kravel <TKravel@Yahoo.com>
2021-10-03 22:09:24 -0400

    form submit script

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('contact-form');
+
+form.onsubmit = (e) => {
+	handleForm(e);
+};
+
+function handleForm(e) {
+	e.preventDefault();
+
+	const email = document.getElementById('email').value;
+	const fullName = document.getElementById('fullName').value;
+	const message = document.getElementById('message').value;
+
+	const data = {
+		email: email,
+		fullName: fullName,
+		message: message,
+	};
+
+	console.log(data);
+
+	fetch('https://young-sierra-17248.herokuapp.com/contact', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	})
+		.then((Response) => Response.json())
+		.then((data) => {
+			console.log(data);
+			email.innerText = '';
+			fullName.innerText = '';
+			message.innerText = '';
+		})
+		.catch((err) => {
+			if (err) {
+				console.log(err);
+			}
+		});
+}
