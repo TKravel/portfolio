556f71f241107c44f1f9381bd789467bae88634f
Tim Kravel <TKravel@Yahoo.com>
2021-12-25 01:13:49 -0500

    Change fetch to https

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,7 @@ function openNav() {
 
 const download = () => {
 	//
-	fetch('http://young-sierra-17248.herokuapp.com/contact/downloads.pfd', {
+	fetch('https://young-sierra-17248.herokuapp.com/contact/downloads.pfd', {
 		method: 'GET',
 	})
 		.then((response) => response.blob())
